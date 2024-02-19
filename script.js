e9963f0c49fc78689b358e5501f3e9567ebb5480
MdAzarudeen <142100127+MdAzarudeen@users.noreply.github.com>
2024-02-19 22:26:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function stringChop(str, size) {
   // your code here
+
+  if (str === null) {
+    return [];
+  }
+  
+  if (str.length === 0) {
+    return [];
+  }
+
+  size = parseInt(size);
+
+  const arr = [];
+  let i = 0;
+
+  while (i < str.length) {
+    arr.push(str.slice(i, i + size));
+    i += size;
+  }
+
+  return arr;
 }
 
 // Do not change the code below
